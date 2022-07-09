ca3fbc35e4b1ce331a8931ff05bd396c8412ad80
Alpha690 <manumuuo@gmail.com>
2022-07-09 12:35:11 +0300

    commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Reads text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    reads file and prints it to STDOUT
+    """
+    with open(filename, encoding="utf-8") as my_file:
+        print(my_file.read(), end="")
